670a6eb6e1d15ef9f9424ef43f18a8f0b184e9a9
TeePlunder <leon.bergmann23@gmail.com>
2023-03-03 20:49:32 +0100

    add brew formulare

--- .install.sh
+++ .install.sh
@@ -3,3 +3,18 @@ echo "Installing Brew..."
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 brew analytics off
 
+# Brew Formulare
+echo "Installing Brew Formulare..."
+brew install neofetch
+brew install lazygit
+brew install lazydocker
+brew install jq
+brew install sketchybar
+brew install zsh-autosuggestions
+brew install zsh-syntax-highlighting
+
+# Brew Casks
+echo "Installing Brew Casks..."
+brew install --cask spotify
+brew install --cask font-hack-nerd-font
+
