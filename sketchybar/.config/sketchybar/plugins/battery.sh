67b4d7ccdaf4ccdadcaa57ffee375cf965468591
TeePlunder <leon.bergmann23@gmail.com>
2023-03-04 15:39:01 +0100

    change battery icons and add charging options

--- sketchybar/.config/sketchybar/plugins/battery.sh
+++ sketchybar/.config/sketchybar/plugins/battery.sh
@@ -7,26 +7,58 @@ BATTERY_INFO="$(pmset -g batt)"
 PERCENTAGE=$(echo "$BATTERY_INFO" | grep -Eo "\d+%" | cut -d% -f1)
 CHARGING=$(echo "$BATTERY_INFO" | grep 'AC Power')
 
-if [ $PERCENTAGE = "" ]; then
+if [ "$PERCENTAGE" = "" ]; then
   exit 0
 fi
 
 DRAWING=on
 COLOR=$WHITE
-case ${PERCENTAGE} in
-  9[0-9]|100) ICON=$BATTERY_100; COLOR=$GREEN
-  ;;
-  [6-8][0-9]) ICON=$BATTERY_75; COLOR=$GREEN
-  ;;
-  [3-5][0-9]) ICON=$BATTERY_50; COLOR=$YELLOW
-  ;;
-  [1-2][0-9]) ICON=$BATTERY_25; COLOR=$ORANGE
-  ;;
-  *) ICON=$BATTERY_0; COLOR=$RED
-esac
+BATTERY_STATUS="BATTERY_"
 
 if [[ $CHARGING != "" ]]; then
-  ICON=$BATTERY_CHARGING
+  BATTERY_STATUS="${BATTERY_STATUS}CHARGING_"
 fi
+case ${PERCENTAGE} in
+100)
+  ICON="${BATTERY_STATUS}100"
+  COLOR=$GREEN
+  ;;
+9[0-9])
+  ICON="${BATTERY_STATUS}90"
+  COLOR=$GREEN
+  ;;
+8[0-9])
+  ICON="${BATTERY_STATUS}80"
+  COLOR=$GREEN
+  ;;
+7[0-9])
+  ICON="${BATTERY_STATUS}70"
+  COLOR=$GREEN
+  ;;
+6[0-9])
+  ICON="${BATTERY_STATUS}60"
+  COLOR=$GREEN
+  ;;
+5[0-9])
+  ICON="${BATTERY_STATUS}50"
+  COLOR=$GREEN
+  ;;
+4[0-9])
+  ICON="${BATTERY_STATUS}40"
+  COLOR=$GREEN
+  ;;
+3[0-9])
+  ICON="${BATTERY_STATUS}30"
+  COLOR=$GREEN
+  ;;
+2[0-9])
+  ICON="${BATTERY_STATUS}20"
+  COLOR=$GREEN
+  ;;
+1[0-9])
+  ICON="${BATTERY_STATUS}10"
+  COLOR=$GREEN
+  ;;
+esac
 
-sketchybar --set $NAME drawing=$DRAWING icon="$ICON" icon.color=$COLOR label=$PERCENTAGE
+sketchybar --set "$NAME" drawing=$DRAWING icon="${!ICON}" icon.color="$COLOR" label="$PERCENTAGE"
