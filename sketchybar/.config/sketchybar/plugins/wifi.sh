72b9c2bbabbe41ea1af66d16dca80a43e8ee169c
TeePlunder <leon.bergmann23@gmail.com>
2023-03-04 13:13:02 +0100

    add wifi icons

--- sketchybar/.config/sketchybar/plugins/wifi.sh
+++ sketchybar/.config/sketchybar/plugins/wifi.sh
@@ -1,16 +1,18 @@
 #!/usr/bin/env sh
 
+source "$HOME/.config/sketchybar/icons.sh"
 CURRENT_WIFI="$(/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I)"
 SSID="$(echo "$CURRENT_WIFI" | grep -o "SSID: .*" | sed 's/^SSID: //')"
 CURR_TX="$(echo "$CURRENT_WIFI" | grep -o "lastTxRate: .*" | sed 's/^lastTxRate: //')"
 
 if [ "$SSID" = "" ]; then
-  sketchybar --set $NAME label="Disconnected" icon=睊
+  sketchybar --set $NAME label="Disconnected" icon=$WIFI_DISCONNECTED
 else
-  sketchybar --set $NAME label="$SSID (${CURR_TX}Mbps)" icon=直
+  sketchybar --set $NAME label="$SSID (${CURR_TX}Mbps)" icon=$WIFI_CONNECTED
 fi
 
 sketchybar --add item wifi right                         \
-           --set wifi    script="$PLUGIN_DIR/wifi.sh"    \
-                         background.padding_right=5     \
+           --set wifi    script="wifi.sh"                \
+                         background.padding_right=10     \
                          update_freq=5
+
