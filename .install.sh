ccd4e0d5df4cbe2a27fb155d7a9d60bb3abe2b21
TeePlunder <leon.bergmann23@gmail.com>
2023-06-25 12:07:05 +0200

    add nerd fonts install

--- .install.sh
+++ .install.sh
@@ -22,14 +22,68 @@ brew install zellij
 # Brew Casks
 echo "Installing Brew Casks..."
 brew install --cask spotify
-brew install --cask font-hack-nerd-font # for sketchybar
-brew install --cask sf-symbols          # for sketchybar
-brew install --cask font-jetbrains-mono-nerd-font
+brew install --cask sf-symbols # for sketchybar
 brew install --cask unnaturalscrollwheels
 
-# Git Installs
-run git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+echo "Installing nerd fonts..."
+fonts_list=(
+  font-3270-nerd-font
+  font-fira-mono-nerd-font
+  font-inconsolata-go-nerd-font
+  font-inconsolata-lgc-nerd-font
+  font-inconsolata-nerd-font
+  font-monofur-nerd-font
+  font-overpass-nerd-font
+  font-ubuntu-mono-nerd-font
+  font-agave-nerd-font
+  font-arimo-nerd-font
+  font-anonymice-nerd-font
+  font-aurulent-sans-mono-nerd-font
+  font-bigblue-terminal-nerd-font
+  font-bitstream-vera-sans-mono-nerd-font
+  font-blex-mono-nerd-font
+  font-caskaydia-cove-nerd-font
+  font-code-new-roman-nerd-font
+  font-cousine-nerd-font
+  font-daddy-time-mono-nerd-font
+  font-dejavu-sans-mono-nerd-font
+  font-droid-sans-mono-nerd-font
+  font-fantasque-sans-mono-nerd-font
+  font-fira-code-nerd-font
+  font-go-mono-nerd-font
+  font-gohufont-nerd-font
+  font-hack-nerd-font # for sketchybar
+  font-hasklug-nerd-font
+  font-heavy-data-nerd-font
+  font-hurmit-nerd-font
+  font-im-writing-nerd-font
+  font-iosevka-nerd-font
+  font-jetbrains-mono-nerd-font
+  font-lekton-nerd-font
+  font-liberation-nerd-font
+  font-meslo-lg-nerd-font
+  font-monoid-nerd-font
+  font-mononoki-nerd-font
+  font-mplus-nerd-font
+  font-noto-nerd-font
+  font-open-dyslexic-nerd-font
+  font-profont-nerd-font
+  font-proggy-clean-tt-nerd-font
+  font-roboto-mono-nerd-font
+  font-sauce-code-pro-nerd-font
+  font-shure-tech-mono-nerd-font
+  font-space-mono-nerd-font
+  font-terminess-ttf-nerd-font
+  font-tinos-nerd-font
+  font-ubuntu-nerd-font
+  font-victor-mono-nerd-font
+)
 
+brew tap homebrew/cask-fonts
+
+for font in "${fonts_list[@]}"; do
+  brew install --cask "$font"
+done
 
 # macOS Settings
 echo "Changing macOS defaults..."
