1644997b5c2f74be579aa031d3e6487ea887e654
TeePlunder <leon.bergmann23@gmail.com>
2023-10-04 12:34:25 +0200

    add treesitter

--- nvim/.config/nvim/lua/teeplunder/packer.lua
+++ nvim/.config/nvim/lua/teeplunder/packer.lua
@@ -14,4 +14,7 @@ return require('packer').startup(function(use)
   }
   use { "ellisonleao/gruvbox.nvim" }
 
+  use ('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+  use ('nvim-treesitter/playground')
+
 end)
