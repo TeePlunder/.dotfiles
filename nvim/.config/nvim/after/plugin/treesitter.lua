9a33e792dabdc7094e64b346121c3f40f4ae5618
TeePlunder <leon.bergmann23@gmail.com>
2023-10-06 14:58:37 +0200

    change treesitter

--- nvim/.config/nvim/after/plugin/treesitter.lua
+++ nvim/.config/nvim/after/plugin/treesitter.lua
@@ -1,4 +1,4 @@
-require'nvim-treesitter.configs'.setup {
+require('nvim-treesitter.configs').setup {
   -- A list of parser names, or "all" (the five listed parsers should always be installed)
   ensure_installed = { "javascript", "typescript", "c", "lua" },
 
@@ -9,13 +9,32 @@ require'nvim-treesitter.configs'.setup {
   -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
   auto_install = true,
 
-  highlight = {
-    enable = true,
+  -- highlight = {
+  --   enable = true,
+  --
+  --   -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+  --   -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+  --   -- Using this option may slow down your editor, and you may see some duplicate highlights.
+  --   -- Instead of true it can also be a list of languages
+  --   additional_vim_regex_highlighting = false,
+  -- },
 
-    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-    -- Using this option may slow down your editor, and you may see some duplicate highlights.
-    -- Instead of true it can also be a list of languages
-    additional_vim_regex_highlighting = false,
+  highlight = {
+      enable = true,
+
+      -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+      -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+      -- Using this option may slow down your editor, and you may see some duplicate highlights.
+      -- Instead of true it can also be a list of languages
+      additional_vim_regex_highlighting = true,
+      custom_captures = {
+          ["punctuation.bracket"] = "",
+          ["constructor"] = "",
+      }
+  },
+  rainbow = {
+      enable = true,
+      extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
+      max_file_lines = nil, -- Do not enable for files with more than n lines, int
   },
 }
