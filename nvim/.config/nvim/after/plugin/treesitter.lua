84c8eba245c900fd9fa28940f36a2182bb65a7d3
TeePlunder <leon.bergmann23@gmail.com>
2023-10-08 11:50:55 +0200

    update treesitter

--- nvim/.config/nvim/after/plugin/treesitter.lua
+++ nvim/.config/nvim/after/plugin/treesitter.lua
@@ -9,16 +9,6 @@ require('nvim-treesitter.configs').setup {
   -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
   auto_install = true,
 
-  -- highlight = {
-  --   enable = true,
-  --
-  --   -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-  --   -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-  --   -- Using this option may slow down your editor, and you may see some duplicate highlights.
-  --   -- Instead of true it can also be a list of languages
-  --   additional_vim_regex_highlighting = false,
-  -- },
-
   highlight = {
       enable = true,
 
@@ -26,7 +16,7 @@ require('nvim-treesitter.configs').setup {
       -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
       -- Using this option may slow down your editor, and you may see some duplicate highlights.
       -- Instead of true it can also be a list of languages
-      additional_vim_regex_highlighting = true,
+      additional_vim_regex_highlighting = false,
       custom_captures = {
           ["punctuation.bracket"] = "",
           ["constructor"] = "",
