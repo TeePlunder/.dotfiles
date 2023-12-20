43931d9ca5b085f1460fb544ff784ac918f2b44b
TeePlunder <leon.bergmann23@gmail.com>
2023-12-20 08:42:39 +0100

    update harpoon

--- nvim/.config/nvim/lua/teeplunder/packer.lua
+++ nvim/.config/nvim/lua/teeplunder/packer.lua
@@ -4,97 +4,100 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-  -- Packer can manage itself
-  use('wbthomason/packer.nvim')
+    -- Packer can manage itself
+    use('wbthomason/packer.nvim')
 
-  use {
-    'nvim-telescope/telescope.nvim', tag = '0.1.3',
-    -- or                            , branch = '0.1.x',
-    requires = { { 'nvim-lua/plenary.nvim' } }
-  }
-  use { "ellisonleao/gruvbox.nvim" }
-
-  use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
-  use('nvim-treesitter/playground')
-  use('theprimeagen/harpoon')
-  use('mbbill/undotree')
-  use('tpope/vim-fugitive')
-  use {
-    'VonHeikemen/lsp-zero.nvim',
-    branch = 'v3.x',
-    requires = {
-      --- Uncomment these if you want to manage LSP servers from neovim
-
-      -- LSP Support
-      { 'neovim/nvim-lspconfig' },
-      { 'williamboman/mason.nvim' },
-      { 'williamboman/mason-lspconfig.nvim' },
-      -- Autocompletion
-      { 'hrsh7th/nvim-cmp' },
-      -- {'hrsh7th/cpm-buffer'},
-      -- {'hrsh7th/cpm-path'},
-      { 'hrsh7th/cmp-nvim-lsp' },
-      { 'hrsh7th/cmp-nvim-lua' },
-      -- Snippets
-      { 'L3MON4D3/LuaSnip' },
-      { 'rafamadriz/friendly-snippets' }
+    use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.3',
+        -- or                            , branch = '0.1.x',
+        requires = { { 'nvim-lua/plenary.nvim' } }
     }
-  }
+    use { "ellisonleao/gruvbox.nvim" }
 
-  use({
-    "kdheepak/lazygit.nvim",
-    -- optional for floating window border decoration
-    requires = {
-      "nvim-lua/plenary.nvim",
-    },
-  })
-  use({
-    'numToStr/Comment.nvim',
-    config = function()
-      require('Comment').setup()
-    end
-  })
-  use({
-    "jose-elias-alvarez/null-ls.nvim",
-    config = function()
-      local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-      local null_ls = require("null-ls")
+    use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
+    use('nvim-treesitter/playground')
+    use "nvim-lua/plenary.nvim" -- don't forget to add this one if you don't have it yet!
+    use {
+        "ThePrimeagen/harpoon",
+        branch = "harpoon2",
+        requires = { { "nvim-lua/plenary.nvim" } }
+    }
+    use('mbbill/undotree')
+    use('tpope/vim-fugitive')
+    use {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v3.x',
+        requires = {
+            --- Uncomment these if you want to manage LSP servers from neovim
 
-      local opts = {
-        sources = {
-          null_ls.builtins.formatting.clang_format
+            -- LSP Support
+            { 'neovim/nvim-lspconfig' },
+            { 'williamboman/mason.nvim' },
+            { 'williamboman/mason-lspconfig.nvim' },
+            -- Autocompletion
+            { 'hrsh7th/nvim-cmp' },
+            { 'hrsh7th/cmp-nvim-lsp' },
+            { 'hrsh7th/cmp-nvim-lua' },
+            -- Snippets
+            { 'L3MON4D3/LuaSnip' },
+            { 'rafamadriz/friendly-snippets' }
+        }
+    }
+
+    use({
+        "kdheepak/lazygit.nvim",
+        -- optional for floating window border decoration
+        requires = {
+            "nvim-lua/plenary.nvim",
         },
-        on_attach = function(client, bufnr)
-          if client.supports_methodd("textDocument/formatting") then
-            vim.api.nvim_clear_autocmds({
-              grouo = augroup,
-              buffer = bufnr,
-            })
-            vim.api.nvim_create_autocmd("BufWritePre", {
-              group = augroup,
-              buffer = bufnr,
-              callback = function()
-                vim.lsp.buf.format({ bufnur = bufnur })
-              end,
-            })
-          end
+    })
+    use({
+        'numToStr/Comment.nvim',
+        config = function()
+            require('Comment').setup()
         end
-      }
-      return opts
-    end,
-    requires = { "nvim-lua/plenary.nvim" },
-  })
-  use {
-    "williamboman/mason.nvim",
-    "jay-babu/mason-nvim-dap.nvim",
-  }
-  use {
-    'nvim-lualine/lualine.nvim',
-    requires = { 'nvim-tree/nvim-web-devicons', opt = true }
-  }
-  use {
-    "folke/twilight.nvim", 'folke/zen-mode.nvim'
-  }
-  use 'neovim/nvim-lspconfig'
-  use 'sigmasd/deno-nvim'
+    })
+    use({
+        "jose-elias-alvarez/null-ls.nvim",
+        config = function()
+            local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+            local null_ls = require("null-ls")
+
+            local opts = {
+                sources = {
+                    null_ls.builtins.formatting.clang_format
+                },
+                on_attach = function(client, bufnr)
+                    if client.supports_methodd("textDocument/formatting") then
+                        vim.api.nvim_clear_autocmds({
+                            grouo = augroup,
+                            buffer = bufnr,
+                        })
+                        vim.api.nvim_create_autocmd("BufWritePre", {
+                            group = augroup,
+                            buffer = bufnr,
+                            callback = function()
+                                vim.lsp.buf.format({ bufnur = bufnur })
+                            end,
+                        })
+                    end
+                end
+            }
+            return opts
+        end,
+        requires = { "nvim-lua/plenary.nvim" },
+    })
+    use {
+        "williamboman/mason.nvim",
+        "jay-babu/mason-nvim-dap.nvim",
+    }
+    use {
+        'nvim-lualine/lualine.nvim',
+        requires = { 'nvim-tree/nvim-web-devicons', opt = true }
+    }
+    use {
+        "folke/twilight.nvim", 'folke/zen-mode.nvim'
+    }
+    use 'neovim/nvim-lspconfig'
+    use 'sigmasd/deno-nvim'
 end)
