b0aeab0d5267b22c91a310b8d469cd045f85c2ac
TeePlunder <leon.bergmann23@gmail.com>
2023-12-29 11:26:49 +0100

    mason

---
+++ nvim/.config/nvim/after/plugin/mason.lua
@@ -0,0 +1,13 @@
+require('mason').setup {
+    ensure_installed = {
+        'bash-language-server',
+        'lua-language-server',
+        'vim-language-server',
+        'stylua',
+        'shellcheck',
+        'shfmt',
+        'clang-format',
+        'clanglldb',
+        'prettier',
+    }
+}
