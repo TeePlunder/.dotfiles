05a85cad5ce1e8e444b1b2e4034688de6e2a05ae
TeePlunder <leon.bergmann23@gmail.com>
2024-12-19 15:50:07 +0100

    Revert "feat: add config file for mult machine usage, private and work"
    This reverts commit 00c38cbafeb6e9e999b99e088b0756c19d82327f.

--- .config/nix/flake.nix
+++ .config/nix/flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Multi-machine nix-darwin system flake with shared and host-specific configs";
+  description = "TeePlunder nix-darwin system flake";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
@@ -8,36 +8,37 @@
     nix-homebrew.url = "github:zhaofengli-wip/nix-homebrew";
   };
 
-  outputs = inputs@{ self, nix-darwin, nixpkgs, nix-homebrew, ... }: let
+  outputs = inputs@{ self, nix-darwin, nixpkgs, nix-homebrew, ... }:
+  let
+    configuration = { pkgs, config, ... }: {
+      nixpkgs.config.allowUnfree = true; # allow to install unfree apps 
 
-    # Base configuration common to all machines
-    baseConfiguration = { pkgs, config, ... }: {
-      nixpkgs.config.allowUnfree = true;
-
-      environment.systemPackages = [
-        pkgs.alacritty
-        pkgs.mkalias
-        pkgs.neovim
-        pkgs.zellij
-        pkgs.lazygit
-        pkgs.lazydocker
-        pkgs.fzf
-        pkgs.bat
-        pkgs.raycast
-        pkgs.obsidian
-        pkgs.vscode
-      ];
+      # List packages installed in system profile. To search by name, run:
+      # $ nix-env -qaP | grep wget
+      environment.systemPackages =
+        [ 
+            pkgs.alacritty
+            pkgs.mkalias
+            pkgs.neovim
+            pkgs.zellij
+            pkgs.lazygit
+            pkgs.lazydocker
+            pkgs.fzf
+            pkgs.bat
+            pkgs.raycast
+            pkgs.obsidian
+        ];
 
       homebrew = {
         enable = true;
         brews = [
-          "mas"
+          "mas" # to install apps from the app store through the cli
           "bruno-cli"
         ];
         casks = [
-          # "hammerspoon"
-          # "iina"
-          # "the-unarchiver"
+          "hammerspoon"
+          "iina"
+          "the-unarchiver"
           "Spark"
           "shottr"
           "alt-tab"
@@ -47,6 +48,9 @@
           "zen-browser"
           "bruno"
         ];
+        masApps = {
+            "Phiewer" = 1226444549;
+        };
         onActivation.cleanup = "zap";
         onActivation.autoUpdate = true;
         onActivation.upgrade = true;
@@ -56,14 +60,16 @@
         (pkgs.nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
       ];
 
-      # Fixing Mac Spotlight to get nix applications like Alacritty
+      # Fixing Mac Spotlight to get the nix applications like alacritty 
       system.activationScripts.applications.text = let
         env = pkgs.buildEnv {
           name = "system-applications";
           paths = config.environment.systemPackages;
           pathsToLink = "/Applications";
         };
-      in pkgs.lib.mkForce ''
+      in
+        pkgs.lib.mkForce ''
+        # Set up applications.
         echo "setting up /Applications..." >&2
         rm -rf /Applications/Nix\ Apps
         mkdir -p /Applications/Nix\ Apps
@@ -73,7 +79,8 @@
           echo "copying $src" >&2
           ${pkgs.mkalias}/bin/mkalias "$src" "/Applications/Nix Apps/$app_name"
         done
-      '';
+            '';
+
 
       system.defaults = {
         dock.autohide = true;
@@ -86,91 +93,59 @@
         ];
         finder.FXPreferredViewStyle = "clmv";
         finder.AppleShowAllExtensions = true;
-        finder.ShowPathbar = true;
-        finder.ShowStatusBar = true;
         loginwindow.GuestEnabled = false;
         NSGlobalDomain.AppleICUForce24HourTime = true;
         NSGlobalDomain.AppleInterfaceStyle = "Dark";
         NSGlobalDomain.KeyRepeat = 2;
       };
 
+      # Auto upgrade nix package and the daemon service.
       services.nix-daemon.enable = true;
+      # nix.package = pkgs.nix;
 
+      # Necessary for using flakes on this system.
       nix.settings.experimental-features = "nix-command flakes";
 
+      # Enable alternative shell support in nix-darwin.
       programs.fish.enable = true;
 
+      # Set Git commit hash for darwin-version.
       system.configurationRevision = self.rev or self.dirtyRev or null;
+
+      # Used for backwards compatibility, please read the changelog before changing.
+      # $ darwin-rebuild changelog
       system.stateVersion = 5;
+
+      # The platform the configuration will be used on.
       nixpkgs.hostPlatform = "aarch64-darwin";
     };
-
-    # Configuration overrides for the private machine
-    privateConfiguration = { pkgs, config, ... }: {
-      # Here you could add packages or settings unique to your private machine.
-      # For example, private-only packages:
-      masApps = {
-        "Phiewer" = 1226444549;
-      };
-      environment.systemPackages = config.environment.systemPackages ++ [
-        pkgs.htop
-        pkgs.pass
-      ];
-    };
-
-    # Configuration overrides for the work machine
-    workConfiguration = { pkgs, config, ... }: {
-      # Add or override packages for the work environment
-      environment.systemPackages = config.environment.systemPackages ++ [
-        pkgs.azure-cli
-      ];
-
-      # # Possibly different defaults for the dock or finder for work
-      # system.defaults = pkgs.lib.mkMerge [
-      #   config.system.defaults
-      #   {
-      #     dock.persistent-apps = [
-      #       # maybe you need different apps in your dock for work
-      #       "/Applications/Visual Studio Code.app"
-      #       "${pkgs.obsidian}/Applications/Obsidian.app"
-      #     ];
-      #   }
-      # ];
-    };
-
-    nixHomebrewModuleForUser = { user }:
-          [
-            nix-homebrew.darwinModules.nix-homebrew
-            {
-              nix-homebrew = {
-                enable = true;
-                enableRosetta = true;
-                user = user;
-                autoMigrate = true;
-              };
-            }
-          ];
-
-  in {
-    # Private configuration
+  in
+  {
+    # Build darwin flake using:
+    # $ darwin-rebuild build --flake .#simple
     darwinConfigurations."private" = nix-darwin.lib.darwinSystem {
-      modules = [
-        baseConfiguration
-        privateConfiguration
-        (nixHomebrewModuleForUser { user = "leonbergmann"; })
-      ];
+      modules = [ 
+          configuration
+          nix-homebrew.darwinModules.nix-homebrew
+          {
+            nix-homebrew = {
+              # Install Homebrew under the default prefix
+              enable = true;
+
+              # Apple Silicon Only: Also install Homebrew under the default Intel prefix for Rosetta 2
+              enableRosetta = true;
+
+              # User owning the Homebrew prefix
+              user = "leonbergmann";
+
+              # Automatically migrate existing Homebrew installations
+              autoMigrate = true;
+            };
+          }
+        ];
     };
 
-    # Work configuration
-    darwinConfigurations."work" = nix-darwin.lib.darwinSystem {
-      modules = [
-        baseConfiguration
-        workConfiguration
-        (nixHomebrewModuleForUser { user = "TODO"; })
-      ];
-    };
-
-    # Expose the package sets for convenience
+    # Expose the package set, including overlays, for convenience.
     darwinPackages = self.darwinConfigurations."private".pkgs;
   };
 }
