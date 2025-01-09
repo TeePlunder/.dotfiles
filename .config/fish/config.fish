0c55177a609602e616475cc013813ca54d11c583
TeePlunder <leon.bergmann23@gmail.com>
2025-01-09 08:38:52 +0100

    feat(fish): add function alias to see ressources, get pids quickly and kill processes quickly

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -10,3 +10,16 @@ test -s ~/.config/envman/load.fish; and source ~/.config/envman/load.fish
 set -Ux fish_user_paths /opt/homebrew/bin $HOME/go/bin $fish_user_paths
 
 #set -gx NVM_DIR (brew --prefix nvm)
+
+function psgrep
+    pgrep -d ',' $argv | xargs -I _ ps -fp _
+end
+
+function topgrep
+    htop -p (pgrep -d ',' $argv)
+end
+
+function pkillgrep
+    pgrep $argv | xargs -I _ kill -9 _
+end
+
