31ff500c5d2b8900e2bace2c7acf142ec19f2944
TeePlunder <leon.bergmann23@gmail.com>
2025-01-15 15:29:58 +0100

    feat(fish): add function to search your history with fzf

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,3 +8,12 @@ zoxide init fish | source
 test -s ~/.config/envman/load.fish; and source ~/.config/envman/load.fish
 
 set -Ux fish_user_paths $fish_user_paths $HOME/go/bin
+
+function fzf_history
+  history | fzf --height=40% --reverse --preview="echo {}" | read -l cmd
+      if test -n "$cmd"
+          eval $cmd
+      end
+end
+
+alias fh=fzf_history
