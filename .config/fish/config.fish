ab85b9964977ac60668ad52ea1cd9f0688bac580
TeePlunder <leon.bergmann23@gmail.com>
2025-01-24 07:22:57 +0100

    fix: add nvm path before setting the other paths

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,6 +7,8 @@ zoxide init fish | source
 # Generated for envman. Do not edit.
 test -s ~/.config/envman/load.fish; and source ~/.config/envman/load.fish
 
+set -gx PATH ~/.nvm/versions/node/v20.18.1/bin $PATH
+
 set -Ux fish_user_paths /opt/homebrew/bin $HOME/go/bin $fish_user_paths
 
 #set -gx NVM_DIR (brew --prefix nvm)
