0c785ed052d2fa3c0fa895cab1fc1066ae4897b9
TeePlunder <leon.bergmann23@gmail.com>
2026-07-26 11:39:40 +0200

    feat(pnpm): setup pnpm

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -26,3 +26,10 @@ set -gx PATH $PATH /Users/leonbergmann/.lmstudio/bin
 
 fish_add_path ~/.local/bin
 alias db="sqlit"
+
+# pnpm
+set -gx PNPM_HOME "/Users/leonbergmann/Library/pnpm"
+if not string match -q -- "$PNPM_HOME/bin" $PATH
+  set -gx PATH "$PNPM_HOME/bin" $PATH
+end
+# pnpm end
